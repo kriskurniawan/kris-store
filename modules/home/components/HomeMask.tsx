ae3e26b33a5fbae73cf18921ec969fb344a37595
Kris Kurniawan <kris7kurniawan@gmail.com>
2024-01-04 06:29:55 +0900

    Update HomeMask

--- modules/home/components/HomeMask.tsx
+++ modules/home/components/HomeMask.tsx
@@ -1,8 +1,20 @@
+import { Button } from '@/common/components/elements/Button'
 import React from 'react'
 
 const HomeMask = () => {
     return (
-        <div>HomeMask</div>
+        <div className='flex relative mb-3'>
+            <img className='object-cover' src="https://res.cloudinary.com/dnbel4bdy/image/upload/v1704256593/kris-store/yyt9xxump5e4kwxr8huj.png" alt="" />
+            <span className='absolute top-6 left-8 text-xl'>Latest trending</span>
+            <span className='absolute top-12 left-8 text-xl font-semibold'>Electronic items</span>
+            <div className='absolute top-24 left-8'>
+                <Button
+                    className='max-w-full h-10 bg-slate-100 border border-slate-600 px-2 rounded-lg font-semibold text-blue-400'
+                >
+                    Learn more
+                </Button>
+            </div>
+        </div>
     )
 }
 
