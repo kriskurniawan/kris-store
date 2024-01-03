ac4764b7ce998ee56e7e260527c30c1b86d9183e
Kris Kurniawan <kris7kurniawan@gmail.com>
2024-01-03 09:12:15 +0900

    Update HomeBanner

--- modules/home/components/HomeBanner.tsx
+++ modules/home/components/HomeBanner.tsx
@@ -5,12 +5,14 @@ const HomeBanner = () => {
     return (
         <div className='relative'>
             <img src="/assets/images/banner.png" alt="" />
-            <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-            <Button
-                className='max-w-full h-10 bg-slate-100 border border-slate-600 p-2 rounded-lg font-semibold text-blue-400'
-            >
-                Learn more
-            </Button>
+            <span className='absolute top-4 left-8 text-xl'>Latest trending</span>
+            <span className='absolute top-10 left-8 text-xl font-semibold'>Electronic items</span>
+            <div className='absolute top-1/2 left-8 -translate-y-1/2'>
+                <Button
+                    className='max-w-full h-10 bg-slate-100 border border-slate-600 p-2 rounded-lg font-semibold text-blue-400'
+                >
+                    Learn more
+                </Button>
             </div>
         </div>
     )
