92c9ca0dc16855d19f3b3f3a8144b19d1b0977e3
Kris Kurniawan <kris7kurniawan@gmail.com>
2024-01-04 12:10:55 +0900

    Update HomeMask

--- modules/home/components/HomeMask.tsx
+++ modules/home/components/HomeMask.tsx
@@ -4,14 +4,16 @@ import React from 'react'
 const HomeMask = () => {
     return (
         <div className='flex relative mb-3'>
-            <img className='object-cover' src="https://res.cloudinary.com/dnbel4bdy/image/upload/v1704256593/kris-store/yyt9xxump5e4kwxr8huj.png" alt="" />
-            <span className='absolute top-6 left-8 text-xl'>Latest trending</span>
-            <span className='absolute top-12 left-8 text-xl font-semibold'>Electronic items</span>
+            
+            <img className='absolute w-full h-[200px] lg:h-[300px] object-cover object-right opacity-20' src="https://res.cloudinary.com/dnbel4bdy/image/upload/v1704256593/kris-store/yyt9xxump5e4kwxr8huj.png" alt="" />
+            <div className="absolute w-full h-[200px] lg:h-[300px] -z-10 bg-gradient-to-r from-blue-700 to-yellow-100" />
+            <span className='absolute top-6 left-8 text-xl text-white'>An easy way to send</span>
+            <span className='absolute top-12 left-8 text-xl font-semibold text-white'>requests to all suppliers</span>
             <div className='absolute top-24 left-8'>
                 <Button
-                    className='max-w-full h-10 bg-slate-100 border border-slate-600 px-2 rounded-lg font-semibold text-blue-400'
+                    className='max-w-full h-10 bg-blue-600 px-2 rounded-lg text-white'
                 >
-                    Learn more
+                    Send inquiry
                 </Button>
             </div>
         </div>
