4d218ff4818e9203e7937256342f069bc6b6e354
Kris Kurniawan <kris7kurniawan@gmail.com>
2024-01-05 06:08:15 +0900

    Update HomeFeature

--- modules/home/components/HomeFeature.tsx
+++ modules/home/components/HomeFeature.tsx
@@ -8,22 +8,22 @@ const HomeFeature = () => {
                     <span className='font-semibold'>Home and outdoor</span>
                 </div>
                 <div className='relative overflow-x-auto'>
-                    <div className="inline-flex">
-                        <div className="flex h-56 w-44 border relative">
+                    <div className="flex">
+                        <div className="flex shrink-0 h-56 w-44 border relative">
                             <div className="gap-2 flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                                 <img className='w-24 h-24' src="https://res.cloudinary.com/dnbel4bdy/image/upload/v1704252318/kris-store/ipgpubzf62fl7uagkuzt.jpg" alt="" />
                                 <span className='text-center'>Clothes</span>
                                 <span className='text-center font-normal text-sm text-slate-500'>Form USD 19</span>
                             </div>
                         </div>
-                        <div className="flex h-56 w-44 border relative">
+                        <div className="flex shrink-0 h-56 w-44 border relative">
                             <div className="gap-2 flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                                 <img className='w-24 h-24' src="https://res.cloudinary.com/dnbel4bdy/image/upload/v1704252486/kris-store/s2ik2gdqjvgxid3wlhyx.png" alt="" />
                                 <span className='text-center'>Clothes</span>
                                 <span className='text-center font-normal text-sm text-slate-500'>Form USD 19</span>
                             </div>
                         </div>
-                        <div className="flex h-56 w-44 border relative">
+                        <div className="flex shrink-0 h-56 w-44 border relative">
                             <div className="gap-2 flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                                 <img className='w-24 h-24' src="https://res.cloudinary.com/dnbel4bdy/image/upload/v1704252372/kris-store/vi6fecnlexutmwxtfsoy.png" alt="" />
                                 <span className='text-center'>Clothes</span>
@@ -42,22 +42,22 @@ const HomeFeature = () => {
                     <span className='font-semibold'>Consumer electronics</span>
                 </div>
                 <div className='relative overflow-x-auto'>
-                    <div className="inline-flex">
-                        <div className="flex h-56 w-44 border relative">
+                    <div className="flex">
+                        <div className="flex shrink-0 h-56 w-44 border relative">
                             <div className="gap-2 flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                                 <img className='w-24 h-24' src="https://res.cloudinary.com/dnbel4bdy/image/upload/v1704256590/kris-store/msftp0eeck4oablglala.png" alt="" />
                                 <span className='text-center'>Clothes</span>
                                 <span className='text-center font-normal text-sm text-slate-500'>Form USD 19</span>
                             </div>
                         </div>
-                        <div className="flex h-56 w-44 border relative">
+                        <div className="flex shrink-0 h-56 w-44 border relative">
                             <div className="gap-2 flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                                 <img className='w-24 h-24' src="https://res.cloudinary.com/dnbel4bdy/image/upload/v1704256589/kris-store/sxev2grn5nyhtnqqaucp.png" alt="" />
                                 <span className='text-center'>Clothes</span>
                                 <span className='text-center font-normal text-sm text-slate-500'>Form USD 19</span>
                             </div>
                         </div>
-                        <div className="flex h-56 w-44 border relative">
+                        <div className="flex shrink-0 h-56 w-44 border relative">
                             <div className="gap-2 flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                                 <img className='w-24 h-24' src="https://res.cloudinary.com/dnbel4bdy/image/upload/v1704256590/kris-store/kyfbnsl6sch5ophxz445.png" alt="" />
                                 <span className='text-center'>Clothes</span>
