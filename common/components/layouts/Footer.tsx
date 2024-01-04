7115e1e7ad28d1854552b5d54963bc2711f067bb
Kris Kurniawan <kris7kurniawan@gmail.com>
2024-01-05 05:54:06 +0900

    Update Footer

--- common/components/layouts/Footer.tsx
+++ common/components/layouts/Footer.tsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='flex flex-row gap-5 h-14 w-full border sticky bottom-0 z-50 bg-white'>
+        <div className='flex flex-row gap-5 h-12 w-full border sticky bottom-0 z-50 bg-white'>
             <div className="flex flex-col m-auto">
-                <img className='h-6 w-12' src="/assets/icons/home.svg" alt="" />
+                <img className='h-4 w-12' src="/assets/icons/home.svg" alt="" />
                 <span className='text-center text-sm'>Home</span>
             </div>
             <div className="flex flex-col m-auto">
-                <img className='h-6 w-12' src="/assets/icons/grid_view.svg" alt="" />
+                <img className='h-4 w-12' src="/assets/icons/grid_view.svg" alt="" />
                 <span className='text-center text-sm'>Menu</span>
             </div>
             <div className="flex flex-col m-auto">
-                <img className='h-6 w-12' src="/assets/icons/account_circle.svg" alt="" />
+                <img className='h-4 w-12' src="/assets/icons/account_circle.svg" alt="" />
                 <span className='text-center text-sm'>Account</span>
             </div>
             <div className="flex flex-col m-auto">
-                <img className='h-6 w-12' src="/assets/icons/settings.svg" alt="" />
+                <img className='h-4 w-12' src="/assets/icons/settings.svg" alt="" />
                 <span className='text-center text-sm'>Settings</span>
             </div>
         </div>
