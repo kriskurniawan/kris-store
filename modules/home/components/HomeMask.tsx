83f00a075a3e562e738fe488ab09cbef0edceb54
Kris Kurniawan <kris7kurniawan@gmail.com>
2024-01-04 18:09:02 +0900

    Fix HomeMask

--- modules/home/components/HomeMask.tsx
+++ modules/home/components/HomeMask.tsx
@@ -7,7 +7,7 @@ const HomeMask = () => {
             <div className='flex relative mb-3'>
                 <img className='absolute w-full h-[200px] lg:h-[300px] object-cover object-right' src="https://res.cloudinary.com/dnbel4bdy/image/upload/v1704256593/kris-store/yyt9xxump5e4kwxr8huj.png" alt="" />
                 <div className='absolute w-full h-[200px] lg:h-[300px] z-10 bg-gradient-to-r from-blue-700 to-yellow-100 opacity-80' />
-                <div className='w-full h-[200px] lg:h-[300px] z-50'>
+                <div className='w-full h-[200px] lg:h-[300px] z-40'>
                     <span className='absolute top-6 left-8 text-xl text-white h-[200px] lg:h-[300px]'>An easy way to send</span>
                     <span className='absolute top-12 left-8 text-xl font-semibold text-white'>requests to all suppliers</span>
                     <div className='absolute top-24 left-8'>
