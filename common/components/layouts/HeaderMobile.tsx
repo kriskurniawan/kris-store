db2657fe89a99955c29679ec031c10a1012b774f
Kris Kurniawan <kris7kurniawan@gmail.com>
2024-01-07 06:29:05 +0900

    Update HeaderMobile

--- common/components/layouts/HeaderMobile.tsx
+++ common/components/layouts/HeaderMobile.tsx
@@ -1,35 +1,57 @@
-import Link from 'next/link'
-import React from 'react'
-import { InputField } from '../elements/InputField'
+'use client'
+
+import React, { useState } from 'react'
+
+function NavLink({ to, children } : any) {
+    return <a href={to} className={`mx-4`}>
+        {children}
+    </a>
+}
+
+function MobileNav({ open, setOpen } : any) {
+    return (
+        <div className={`absolute top-0 left-0 h-screen w-screen bg-white transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md`}>
+            <div className="flex items-center justify-center filter bg-white h-20 shadow-sm md:hidden"> {/*logo container*/}
+                <a className="text-xl font-semibold" href="/">LOGO</a>
+            </div>
+            <div className="flex flex-col gap-2 p-4">
+                <a className="text-xl font-medium" href="/about" onClick={() => setTimeout(() => { setOpen(!open) }, 100)}>
+                    About
+                </a>
+                <a className="text-xl font-normal" href="/contact" onClick={() => setTimeout(() => { setOpen(!open) }, 100)}>
+                    Contact
+                </a>
+            </div>
+        </div>
+    )
+}
 
 const HeaderMobile = () => {
+
+    const [open, setOpen] = useState(false)
     return (
-        <>
-            <header className='sticky top-0 z-50 pb-2'>
-                <div className="p-5 w-full h-16 flex items-center justify-between bg-white">
-                    <div className="flex items-center gap-5">
-                        <img src="/assets/icons/menu.svg" alt="" />
-                        <img src="/assets/icons/logo.svg" alt="" />
-                    </div>
-                    <div className="flex items-center gap-5">
-                        <Link href="/cart">
-                            <img src="/assets/icons/shopping_cart.svg" alt="" />
-                        </Link>
-                        <img src="/assets/icons/person.svg" alt="" />
-                    </div>
+        <nav className="flex filter bg-white p-5 h-16 items-center sticky top-0 z-50">
+            <MobileNav open={open} setOpen={setOpen} />
+            <div className="flex items-center">
+                <div className="z-50 flex relative w-8 h-6 flex-col justify-between items-center md:hidden" onClick={() => {
+                    setOpen(!open)
+                }}>
+                    {/* hamburger button */}
+                    <span className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-2.5" : ""}`} />
+                    <span className={`h-1 w-full bg-black rounded-lg transition-all duration-300 ease-in-out ${open ? "opacity-0" : ""}`} />
+                    <span className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-2.5" : ""}`} />
                 </div>
-            </header>
-            <div className='px-5 relative'>
-                <img src="/assets/icons/search.svg" alt="" className='absolute top-1/2 -translate-y-1/2 ml-3' style={{ filter: 'brightness(4) invert(1)' }} />
-                <InputField
-                    type="text"
-                    name="search"
-                    id="search"
-                    placeholder='Search'
-                    className="w-full h-12 rounded-lg border border-slate-300 bg-slate-100 pl-10"
-                />
+
+                <div className="hidden md:flex">
+                    <NavLink to="/contact">
+                        CONTACT
+                    </NavLink>
+                    <NavLink to="/about">
+                        ABOUT
+                    </NavLink>
+                </div>
             </div>
-        </>
+        </nav>
     )
 }
 
