1ddd78884b8eff82b957777f189f3c2977043c10
Kris Kurniawan <kris7kurniawan@gmail.com>
2024-01-07 07:11:42 +0900

    Fix MobileHeader

--- common/components/layouts/HeaderMobile.tsx
+++ common/components/layouts/HeaderMobile.tsx
@@ -1,18 +1,19 @@
 'use client'
 
+import Link from 'next/link'
 import React, { useState } from 'react'
 
-function NavLink({ to, children } : any) {
+function NavLink({ to, children }: any) {
     return <a href={to} className={`mx-4`}>
         {children}
     </a>
 }
 
-function MobileNav({ open, setOpen } : any) {
+function MobileNav({ open, setOpen }: any) {
     return (
-        <div className={`absolute top-0 left-0 h-screen w-screen bg-white transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md`}>
-            <div className="flex items-center justify-center filter bg-white h-20 shadow-sm md:hidden"> {/*logo container*/}
-                <a className="text-xl font-semibold" href="/">LOGO</a>
+        <div className={`absolute top-0 left-0 h-screen w-full bg-white transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md`}>
+            <div className="flex items-center justify-center filter bg-white h-16 shadow-sm md:hidden"> {/*logo container*/}
+                <img src="/assets/icons/logo.svg" alt="" />
             </div>
             <div className="flex flex-col gap-2 p-4">
                 <a className="text-xl font-medium" href="/about" onClick={() => setTimeout(() => { setOpen(!open) }, 100)}>
@@ -29,8 +30,9 @@ function MobileNav({ open, setOpen } : any) {
 const HeaderMobile = () => {
 
     const [open, setOpen] = useState(false)
+
     return (
-        <nav className="flex filter bg-white p-5 h-16 items-center sticky top-0 z-50">
+        <nav className="flex filter bg-white p-5 h-16 items-center sticky top-0 z-50 justify-between">
             <MobileNav open={open} setOpen={setOpen} />
             <div className="flex items-center">
                 <div className="z-50 flex relative w-8 h-6 flex-col justify-between items-center md:hidden" onClick={() => {
@@ -41,7 +43,7 @@ const HeaderMobile = () => {
                     <span className={`h-1 w-full bg-black rounded-lg transition-all duration-300 ease-in-out ${open ? "opacity-0" : ""}`} />
                     <span className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-2.5" : ""}`} />
                 </div>
-
+                <img className='ml-5' src="/assets/icons/logo.svg" alt="" />
                 <div className="hidden md:flex">
                     <NavLink to="/contact">
                         CONTACT
@@ -50,6 +52,12 @@ const HeaderMobile = () => {
                         ABOUT
                     </NavLink>
                 </div>
+            </div>
+            <div className="flex items-center gap-5">
+                <Link href="/cart">
+                    <img src="/assets/icons/shopping_cart.svg" alt="" />
+                </Link>
+                <img src="/assets/icons/person.svg" alt="" />
             </div>
         </nav>
     )
