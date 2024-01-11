c75ac465fc296923034282a170218ed6f6191b5d
Kris Kurniawan <kris7kurniawan@gmail.com>
2024-01-11 12:38:41 +0900

    Update Footer

--- common/components/layouts/Footer.tsx
+++ common/components/layouts/Footer.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import React from 'react'
+import { Icon } from '@iconify/react';
 
 const Footer = () => {
     return (
@@ -19,6 +22,90 @@ const Footer = () => {
                 <div className="flex flex-col m-auto">
                     <img className='h-4 w-14' src="/assets/icons/settings.svg" alt="" />
                     <span className='text-center text-sm'>Settings</span>
+                </div>
+            </div>
+            <div className="hidden lg:flex bg-white w-full h-56 justify-center items-center">
+                <div className="flex mx-32 gap-16">
+                    <div className="flex h-full w-72 py-5">
+                        <div className="flex flex-col gap-3">
+                            <img className='h-12 w-32' src="/assets/icons/logo.svg" alt="" />
+                            <div className="flex flex-wrap w-full h-12">
+                                <span>Best information about the company gies here but now lorem ipsum is</span>
+                            </div>
+                            <div className="flex gap-3">
+                                <div className="bg-slate-400 rounded-full p-2 text-white">
+                                    <Icon icon="lucide:facebook" width="20" height="20" />
+                                </div>
+                                <div className="bg-slate-400 rounded-full p-2 text-white">
+                                    <Icon icon="lucide:twitter" width="20" height="20" />
+                                </div>
+                                <div className="bg-slate-400 rounded-full p-2 text-white">
+                                    <Icon icon="lucide:linkedin" width="20" height="20" />
+                                </div>
+                                <div className="bg-slate-400 rounded-full p-2 text-white">
+                                    <Icon icon="lucide:instagram" width="20" height="20" />
+                                </div>
+                                <div className="bg-slate-400 rounded-full p-2 text-white">
+                                    <Icon icon="lucide:youtube" width="20" height="20" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex h-full w-28 py-5">
+                        <div className="flex flex-col">
+                            <span className='font-semibold mb-2'>About</span>
+                            <span className='text-slate-400'>About Us</span>
+                            <span className='text-slate-400'>Find store</span>
+                            <span className='text-slate-400'>Categories</span>
+                            <span className='text-slate-400'>Blogs</span>
+                        </div>
+                    </div>
+                    <div className="flex h-full w-28 py-5">
+                        <div className="flex flex-col">
+                            <span className='font-semibold mb-2'>Partnership</span>
+                            <span className='text-slate-400'>About Us</span>
+                            <span className='text-slate-400'>Find store</span>
+                            <span className='text-slate-400'>Categories</span>
+                            <span className='text-slate-400'>Blogs</span>
+                        </div>
+                    </div>
+                    <div className="flex h-full w-28 py-5">
+                        <div className="flex flex-col">
+                            <span className='font-semibold mb-2'>Information</span>
+                            <span className='text-slate-400'>Help Center</span>
+                            <span className='text-slate-400'>Money Refund</span>
+                            <span className='text-slate-400'>Shipping</span>
+                            <span className='text-slate-400'>Contact Us</span>
+                        </div>
+                    </div>
+                    <div className="flex h-full w-28 py-5">
+                        <div className="flex flex-col">
+                            <span className='font-semibold mb-2'>For Users</span>
+                            <span className='text-slate-400'>Login</span>
+                            <span className='text-slate-400'>Register</span>
+                            <span className='text-slate-400'>Settings</span>
+                            <span className='text-slate-400'>My Orders</span>
+                        </div>
+                    </div>
+                    <div className="flex h-full w-28 py-5">
+                        <div className="flex flex-col">
+                            <span className='font-semibold mb-2'>Get App</span>
+                            <span className='text-slate-400'>Apple Store</span>
+                            <span className='text-slate-400'>Google Play</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="hidden lg:flex bg-gray-200 w-full h-16">
+                <div className="flex w-full h-full mx-32 gap-16 justify-between">
+                    <div className="flex items-center">
+                        <span className='text-slate-400'>© 2023 Kris Store</span>
+                    </div>
+                    <div className="flex items-center gap-2 text-slate-600">
+                        <img className='w-8' src="https://res.cloudinary.com/dnbel4bdy/image/upload/v1704944100/kris-store/rbn4ilulffnzpkqkttu3.png" alt="" />
+                        <span>English</span>
+                        <Icon icon="lucide:chevron-up" width="20" height="20" />
+                    </div>
                 </div>
             </div>
         </>
--- modules/home/components/HomeSubscribe.tsx
+++ modules/home/components/HomeSubscribe.tsx
@@ -8,7 +8,7 @@ import { Button } from '@/common/components/elements/Button';
 const HomeSubscribe = () => {
     return (
         <>
-            <div className='hidden lg:block w-full h-56 bg-gray-200 mb-3'>
+            <div className='hidden lg:block w-full h-56 bg-gray-200'>
                 <div className='flex flex-col justify-center items-center p-10 gap-3'>
                     <span className='text-4xl font-semibold'>Subscribe to our newsletter</span>
                     <span className='text-lg font-normal'>Get all the latest information on sales, offers and more</span>
@@ -19,7 +19,7 @@ const HomeSubscribe = () => {
                         id="subscribe" 
                         placeholder='Email' 
                         className="w-96 h-12 rounded-lg border border-slate-300 bg-slate-100 pl-12" />
-                        <Icon icon="lucide:mail" width="24" height="24" className="absolute left-3 top-3"/>
+                        <Icon icon="lucide:mail" width="24" height="24" className="absolute left-3 top-3 text-slate-400"/>
                         <Button className="w-36 h-12 bg-blue-600 rounded-lg text-white">Subscribe</Button>
                     </div>
                 </div>
